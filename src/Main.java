2b8fd8b3e8981ae045120689e2e5955f0f9306e1
Hazal Nur Akyuz <hazalnurakyuz@outlook.com>
2022-08-09 18:29:14 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int sicaklik;
+        System.out.print("Hava Sicakligini Giriniz :");
+        sicaklik = input.nextInt();
+
+        if (sicaklik>=30) {
+            System.out.println("Bu Havada Yuzmeye Gidilir");
+        }else if (sicaklik>=5 && sicaklik<=30) {
+            System.out.println("Bu Havada Sinemaya Gidilir");
+        }else {
+            System.out.println("Bu Havada Kayak Yapmaya Gidilir");
+        }
+
     }
 }
